186dcd9be3791de484278d74ecf073c26d31db3c
Visan Tudor-Cosmin <tudorcosmin.visan@gmail.com>
2021-12-20 06:12:42 +0200

    Add UserController, which contains REST endpoints for operations regarding users.

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,48 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    public UserService userService;
+
+    @GetMapping("/users")
+    public List<User> getAllUsers(){
+        return userService.findAll();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerNewUser(@RequestParam String userName, @RequestParam String userEmail, @RequestParam String password){
+        int result = userService.registerUser(userName,userEmail,password);
+        ResponseEntity<String> res;
+
+        switch(result){
+            case 0:
+                res=new ResponseEntity<String>("Registration successful",HttpStatus.OK);
+                break;
+            case 1:
+                res =new  ResponseEntity<String>("User already exists",HttpStatus.INTERNAL_SERVER_ERROR);
+                break;
+            case 2:
+                res =new  ResponseEntity<String>("Your password is too weak.Please use 8+ characters including digits",HttpStatus.INTERNAL_SERVER_ERROR);
+                break;
+            default:
+                res =new  ResponseEntity<String>("Unknown error",HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return res;
+
+    }
+}
